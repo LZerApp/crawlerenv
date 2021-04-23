19a3bc9d9d3d6363c867b5cd0bd31d113c919aa8
jack77703 <jack881104@gmail.com>
2021-04-23 12:51:55 +0800

    14

--- application/services/tempcrawler.py
+++ application/services/tempcrawler.py
@@ -952,7 +952,7 @@ def Wishbykorea():
                     "//div[@class='collection_item'][%i]/a[@href]" % (i,)).get_attribute('href')
                 page_id = page_link.replace("https://www.wishbykorea.com/collection-view-", "").replace("&ca=727", "")
                 find_href = chrome.find_element_by_xpath(
-                    "//div[@class='collection_item'][%i]/div/ul/li[1]" % (i,))
+                    "//div[@class='collection_item'][%i]/a/div" % (i,))
                 bg_url = find_href.value_of_css_property('background-image')
                 pic_link = bg_url.lstrip('url("').rstrip('")')
             except:
